8ec3231a2b508175dcac17c3ecbc0af5d56ff4db
Evan Wilde <etcwilde@uvic.ca>
2017-10-22 14:34:12 -0700

    Fixing constness and cleaning up spaces

--- tutorials/06_more_classes/lab06/part2/matrix.cpp
+++ tutorials/06_more_classes/lab06/part2/matrix.cpp
@@ -1,7 +1,7 @@
 /* matrix.cpp
-  
+
    CSC 116 Fall 2016 - Lab 6
-   
+
 */
 #include <iostream>
 #include <string>
@@ -10,48 +10,45 @@
 #include "matrix.hpp"
 
 
-
 /* Constructor( num_rows, num_cols )
    Create a matrix of the given size, with all values
    set to zero.
 */
-Matrix::Matrix(unsigned int num_rows, unsigned int num_cols){
+Matrix::Matrix(unsigned int num_rows, unsigned int num_cols) {
 
 }
-
 
 /* Constructor( num_rows, num_cols, initial_value, default_value )
    Create a matrix of the given size, with all values
    set to the default value provided.
 */
-Matrix::Matrix(unsigned int num_rows, unsigned int num_cols, double default_value){
+Matrix::Matrix(unsigned int num_rows, unsigned int num_cols, double default_value) {
 
 }
 
 /* get_num_rows()
    Return the number of rows in the matrix
 */
-unsigned int Matrix::get_num_rows(){
+unsigned int Matrix::get_num_rows() const {
 
 }
 
 /* get_num_cols()
    Return the number of columns in the matrix
 */
-unsigned int Matrix::get_num_cols(){
+unsigned int Matrix::get_num_cols() const {
 
 }
-
 
 /* print()
    Output the matrix to standard output
 */
-void Matrix::print(){
-	for(unsigned int i {0}; i < get_num_rows(); i++){
-		for(unsigned int j {0}; j < get_num_cols(); j++)
-			std::cout << at(i,j) << " ";
-		std::cout << std::endl;
-	}
+void Matrix::print() const {
+    for(unsigned int i {0}; i < get_num_rows(); i++) {
+        for(unsigned int j {0}; j < get_num_cols(); j++)
+            std::cout << at(i,j) << " ";
+        std::cout << std::endl;
+    }
 }
 
 /* at(i,j)
@@ -59,24 +56,34 @@ void Matrix::print(){
    If the provided indices are out of range, throw an instance
    of std::out_of_range with the message "Invalid indices"
 */
-double& Matrix::at(unsigned int i, unsigned int j){
+double& Matrix::at(unsigned int i, unsigned int j) {
 
 }
 
+/* at(i,j)
+   Return a constant reference to the element at row i, column j.
+   If the provided indices are out of range, throw an instance
+   of std::out_of_range with the message "Invalid indices"
+*/
+double const & Matrix::at(unsigned int i, unsigned int j) const {
+
+}
+
+
 /* trace()
    Compute and return the trace of this matrix.
-   
+
    The trace of a square matrix is the sum of the diagonal entries.
    For example, the trace of the matrix
-	  1 2 3
-	  4 5 6
-	  7 8 9
+      1 2 3
+      4 5 6
+      7 8 9
    is 15.
-   
+
    If this matrix is not square, the method must throw a std::domain_error
    with message "Unable to compute trace of a non-square matrix".
 */
-double Matrix::trace(){
+double Matrix::trace() const {
 
 }
 
@@ -84,7 +91,7 @@ double Matrix::trace(){
    Return true if this matrix is square and diagonal (that is, if all of the non-diagonal
    elements are 0) and false otherwise.
 */
-bool Matrix::is_diagonal(){
+bool Matrix::is_diagonal() const {
 
 }
 
@@ -95,22 +102,22 @@ bool Matrix::is_diagonal(){
 /* operator- (other_matrix)
    Compute the difference between this matrix and the provided matrix
    and return the result.
-   
+
    If the provided matrix does not have matching dimensions to this
    matrix, throw a std::domain_error with the message "Incompatible dimensions"
 */
-Matrix Matrix::operator-( Matrix other_matrix ){
+Matrix Matrix::operator-( Matrix const & other_matrix ) const {
 
 }
 
 /* operator+ (other_matrix)
    Compute the sum of this matrix and the provided matrix
    and return the result.
-   
+
    If the provided matrix does not have matching dimensions to this
    matrix, throw a std::domain_error with the message "Incompatible dimensions"
 */
-Matrix Matrix::operator+( Matrix other_matrix ){
+Matrix Matrix::operator+( Matrix const & other_matrix ) const {
 
 }
 
@@ -119,7 +126,7 @@ Matrix Matrix::operator+( Matrix other_matrix ){
    This only covers multiplication by a scalar on the right. Multiplication
    by a scalar on the left cannot be handled by an operator which is part of the class.
 */
-Matrix Matrix::operator*( double scalar ){
+Matrix Matrix::operator*( double scalar ) const {
 
 }
 
@@ -133,7 +140,7 @@ Matrix Matrix::operator*( double scalar ){
    does not have access to the private members of that class, unless you add
    a friend directive for it.
 */
-Matrix operator*( double scalar, Matrix M ){
+Matrix operator*( double scalar, Matrix const &  M ) {
 
 }
 
@@ -141,24 +148,23 @@ Matrix operator*( double scalar, Matrix M ){
    since they have inherent inaccuracies. Instead, use the function below
    to test whether two doubles are equal to within a tolerance of 10^{-7}.
 */
-bool doubles_equal( double a, double b ){
-	return fabs(a-b) < 1e-7;
+bool doubles_equal( double a, double b ) {
+    return fabs(a-b) < 1e-7;
 }
 
 /* operator==( M1, M2 )
    Return true if M1 and M2 have the same dimensions and
    are equal (element-wise) and false otherwise.
-   
+
    Use the doubles_equal function above to compare values.
 */
-bool operator==(Matrix M1, Matrix M2){
-
+bool operator==(Matrix const & M1, Matrix const & M2) {
 
 }
 /* operator!=( M1, M2 )
    Return true if M1 and M2 are not equal. This should
    have the opposite behavior to the == operator.
 */
-bool operator!=(Matrix M1, Matrix M2){
-	
+bool operator!=(Matrix const & M1, Matrix const & M2) {
+
 }
