2fc0ee838130a9dd5571792b63b953d16d8b11d0
Evan Wilde <etcwilde@uvic.ca>
2017-10-29 19:42:40 -0700

    Make test for part2 more rigorous

--- tutorials/07_inheritance/lab07/part2/part2.cpp
+++ tutorials/07_inheritance/lab07/part2/part2.cpp
@@ -15,10 +15,16 @@ int main(int argc, char *argv[]) {
     Point p1(1., 2.);
 
     Rectangle rect(p0, p1);
-
     std::cout << "Rectangle type: " << rect.name() << std::endl;
     std::cout << "Rectangle center: " << rect.center() << std::endl;
     std::cout << "Rectangle dimensions: " << rect.width() << " x " << rect.height() << std::endl;
     std::cout << "Rectangle min and max: " << rect.getMin() << " and " << rect.getMax() << std::endl;
+
+    Rectangle rect1(p1, {3, 3});
+    std::cout << "Rectangle 1 type: " << rect1.name() << std::endl;
+    std::cout << "Rectangle 1 center: " << rect1.center() << std::endl;
+    std::cout << "Rectangle 1 dimensions: " << rect1.width() << " x " << rect1.height() << std::endl;
+    std::cout << "Rectangle 1 min and max: " << rect1.getMin() << " and " << rect1.getMax() << std::endl;
+
     return 0;
 }
